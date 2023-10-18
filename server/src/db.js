5fa576c3e35b1dec8a71578fdc1fba2aff73ed85
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-18 19:00:53 -0300

    feat(server):add structure project

--- server/src/db.js
+++ server/src/db.js
@@ -1,3 +1,9 @@
 import mongoose from 'mongoose'
 
-mongoose.connect()
+const connectDb = async(DB_URL)=>{
+    await mongoose.connect(DB_URL)
+    .then(() => console.log('DB is connected'))
+    .catch((error) => console.log(error))
+}
+
+export default connectDb
--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,13 @@
 import app from './app.js'
+import connectDb from './db.js'
+import dotenv from 'dotenv'
 
-app.listen(3000)
-console.log('Server listening in port', 3000)
+dotenv.config()
+
+if(process.env.DB_URL){
+    connectDb(process.env.DB_URL)
+}
+
+app.listen(3001, () => {
+    console.log('Server listening in port', 3001)
+})
