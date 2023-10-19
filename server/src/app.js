391460207d84c30c9e39803e9683da30fcaa7ecb
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-19 09:58:43 -0300

    feat(server/routes):add basic route schema

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import morgan from 'morgan'
+import router from './routes/routes'
 
 const app = express()
 
 app.use(morgan('dev'))
 
-
+app.use('/api', router)
 
 export default app
