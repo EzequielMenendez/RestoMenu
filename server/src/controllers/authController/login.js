5025d05942b8a1a8ab29f211c8d298904eaa1294
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-20 14:36:46 -0300

    feat(controllers/login):add login controller

--- server/src/controllers/authController/login.js
+++ server/src/controllers/authController/login.js
@@ -1,7 +1,26 @@
+import config from '../../config'
 import User from '../../models/userModel'
+import jwt from 'jsonwebtoken'
 
 const loginController = async(req, res) => {
-    
+    const {email, password} = req.body
+    try {
+       const userFound = await User.findOne({email}).populate("roles")
+        if(!userFound)return res.status(404).json({error: "User not found"})
+
+        const matchPassword = await User.comparePassword(password, userFound.password)
+
+        if(!matchPassword)return res.status(400).json({error: "incorrect password"})
+
+        const token = jwt.sign({id: userFound._id}, config.SECRET, {
+            expiresIn: 86400
+        })
+
+        return res.status(200).json({token})
+
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
 }
 
 export default loginController
