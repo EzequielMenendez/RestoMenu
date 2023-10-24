e7ad08c36d4a83078715cabd8f389fb6a2207d54
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-24 15:02:09 -0300

    feat(authJWT):add error message

--- server/src/middlewares/authJwt.js
+++ server/src/middlewares/authJwt.js
@@ -3,17 +3,21 @@ import config from '../config'
 import User from '../models/userModel'
 
 const verifyToken = async(req, res, next)=>{
-    const token = req.headers["x-access-token"]
+    try {
+        const token = req.headers["x-access-token"]
 
-    if(!token) return res.status(400).json({error: "No token provided"})
-
-    const decoded = jwt.verify(token, config.SECRET)
-
-    await User.findById(decoded.id, {password: 0})
-
-    if(!user) return res.status(404).json({error: "no user found"})
-
-    next()
+        if(!token) return res.status(400).json({error: "No token provided"})
+    
+        const decoded = jwt.verify(token, config.SECRET)
+    
+        await User.findById(decoded.id, {password: 0})
+    
+        if(!user) return res.status(404).json({error: "no user found"})
+    
+        next()
+    } catch (error) {
+        return res.status(500).json({error: "Unauthorized"})
+    }
 }
 
 export default verifyToken
