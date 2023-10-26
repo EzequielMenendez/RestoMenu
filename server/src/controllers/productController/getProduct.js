6e81eb0a9c4a737b76cd5a4927844f3f31d3f32d
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-26 11:17:08 -0300

    feat(controllers): add getProducts

--- server/src/controllers/productController/getProduct.js
+++ server/src/controllers/productController/getProduct.js
@@ -1,7 +1,16 @@
 import Product from '../../models/productModel'
+import Category from '../../models/categoryModel'
 
 const getProduct = async(req, res) => {
-    
+    const { categoryId } = req.body
+    try {
+        const products = await Product.findAll({categories:{$in: categoryId}}).populate('Category')
+
+        if(products.length === 0)return res.status(404).json({error: 'Products not found'})
+        res.status(200).json(products)
+    } catch (error) {
+        return res.status(500).json({error: 'Server error'})
+    }
 }
 
 export default getProduct
