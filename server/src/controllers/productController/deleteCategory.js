1624a472d974ec21f1a412a75ff78b6f2c4e93da
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-30 10:37:08 -0300

    fix(deleteCategory):add delete relations

--- server/src/controllers/productController/deleteCategory.js
+++ server/src/controllers/productController/deleteCategory.js
@@ -1,12 +1,21 @@
 import Category from '../../models/categoryModel'
 import Product from '../../models/productModel'
+import User from '../../models/userModel'
 
 const deleteCategory = async(req, res)=>{
     const {id} = req.params
     try {
-        await Product.deleteMany({ categories: { $in: [id], $size: 1 } })
         const deletedCategory = await Category.findByIdAndDelete(id)
         if(!deletedCategory)return res.status(404).json({error: 'Category not found'})
+        await Product.deleteMany({ categories: { $in: [id], $size: 1 } })
+        await Product.updateMany(
+            { categories: id },
+            { $pull: { categories: id } }
+        )
+        await User.updateMany(
+            { categories: id },
+            { $pull: { categories: id } }
+        )
         return res.status(204).json({message: 'Deleted successfuly'})
     } catch (error) {
         return res.status(500).json({error: error.message})
