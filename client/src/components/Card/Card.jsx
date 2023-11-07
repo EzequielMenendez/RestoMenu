bee61860f4dd9ed0854cb654d682268f8514bd68
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-07 11:58:28 -0300

    feat():add info in Card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,7 +2,9 @@ const Card = (props)=>{
     const {username, image, location} = props
     return (
         <div>
-            <h1>{username}</h1>
+            <img src="https://static.vecteezy.com/system/resources/previews/018/765/757/non_2x/user-profile-icon-in-flat-style-member-avatar-illustration-on-isolated-background-human-permission-sign-business-concept-vector.jpg" width={50}/>
+            <h3>{username}</h3>
+            <p>{location}</p>
         </div>
     )
 }
