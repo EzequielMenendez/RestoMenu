49c8ff5373dd66352195a47063626ee1c4ee3df1
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-07 12:08:22 -0300

    feat(Card): add basic style

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,11 +1,15 @@
+import { CardContainer } from "./StyledCard"
+
 const Card = (props)=>{
     const {username, image, location} = props
     return (
-        <div>
+        <CardContainer>
             <img src="https://static.vecteezy.com/system/resources/previews/018/765/757/non_2x/user-profile-icon-in-flat-style-member-avatar-illustration-on-isolated-background-human-permission-sign-business-concept-vector.jpg" width={50}/>
-            <h3>{username}</h3>
-            <p>{location}</p>
-        </div>
+            <div>
+                <h3>{username}</h3>
+                <p>{location}</p>
+            </div>
+        </CardContainer>
     )
 }
 
---
+++ client/src/components/Card/StyledCard.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const CardContainer = styled.div`
+    display: flex;
+    align-items: center;
+    width: 200px;
+    height: 100px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    margin: 10px;
+`
