73fe12f8ae35ce66e005781a5d8aadfd4fe5fc1d
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-08 12:15:57 -0300

    feat(Register): add register form schema

--- client/src/views/Register/Register.jsx
+++ client/src/views/Register/Register.jsx
@@ -1,7 +1,48 @@
+import { useForm } from 'react-hook-form'
+
 const Register = ()=>{
+
+    const {register, handleSubmit, formState:{errors}} = useForm()
+
+    const onSubmit = (values) =>{
+        
+    }
+
     return (
         <div>
-            <p>Register</p>
+            <p>Register</p> 
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type='text' {...register('username', {required: 'true'})} placeholder='username'/>
+                {errors.username && <p>Name is required</p>}
+                <input
+                    type="email"
+                    id="email"
+                    {...register('email', {
+                    required: 'Email is required',
+                    pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Invalid email address',
+                    },
+                    })}
+                    placeholder="Email"
+                />
+                {errors.email ? (
+                    <p>
+                    {errors.email.type === 'required'
+                    ? 'Email is required'
+                    : errors.email.type === 'pattern'
+                    ? 'Invalid email address'
+                    : null}
+                    </p>
+                ) : null}
+                <input type="password" {...register('password', {required: 'true', minLength: 6})} placeholder='password'/>
+                {errors.password?.type === 'required' ? (
+                    <p className='text-red-500'>password is required</p>
+                ) : errors.password?.type === 'minLength' ? (
+                    <p className='text-red-500'>Password must be at least 6 characters</p>
+                ) : null}
+                <button type='submit'>Register</button>
+            </form>
         </div>
     )
 }
