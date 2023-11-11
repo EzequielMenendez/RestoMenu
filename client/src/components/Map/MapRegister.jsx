f20a4b7fcf4569f9f31e758ecf1f127f25fd5d0a
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-10 21:27:32 -0300

    feat(Markers): add basic marker

--- client/src/components/Map/MapRegister.jsx
+++ client/src/components/Map/MapRegister.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import Markers from '../Marker/Markers'
 
 const MapRegister = () =>{
     return (
@@ -10,6 +11,7 @@ const MapRegister = () =>{
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
+            <Markers />
             </MapContainer>
         </div>
     )
