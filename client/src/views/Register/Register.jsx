684c8d591278fed3a15e78d5d92e998a033f991a
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-30 12:21:17 -0300

    add form contact and location

--- client/src/views/Register/Register.jsx
+++ client/src/views/Register/Register.jsx
@@ -12,8 +12,10 @@ const Register = ()=>{
         <div>
             <p>Register</p> 
             <form onSubmit={handleSubmit(onSubmit)}>
+
                 <input type='text' {...register('username', {required: 'true'})} placeholder='username'/>
                 {errors.username && <p>Name is required</p>}
+
                 <input
                     type="email"
                     id="email"
@@ -35,11 +37,28 @@ const Register = ()=>{
                     : null}
                     </p>
                 ) : null}
+
                 <input type="password" {...register('password', {required: 'true', minLength: 6})} placeholder='password'/>
                 {errors.password?.type === 'required' ? (
                     <p>password is required</p>
                 ) : errors.password?.type === 'minLength' ? (
                     <p>Password must be at least 6 characters</p>
+                ) : null}
+
+                <input type='text' {...register('location', {required: 'true', maxLength: 20})} placeholder='location'/>
+                {errors.location?.type === 'required' ? (
+                    <p>location is required</p>
+                ) : errors.location?.type === 'maxLength' ? (
+                    <p>location cannot be more than 20 characters</p>
+                ) : null}
+
+                <input type="number" {...register('contact', {required: true, minLength: 9, maxLength: 15})} placeholder='contact'/>
+                {errors.contact?.type === 'required' ? (
+                    <p>contact is required</p>
+                ) : errors.contact?.type === 'maxLength' ? (
+                    <p>contact cannot be more than 20 characters</p>
+                ) : errors.contact?.type === 'minLength' ? (
+                    <p>contact must be at least 9 characters</p>
                 ) : null}
                 <button type='submit'>Register</button>
             </form>
